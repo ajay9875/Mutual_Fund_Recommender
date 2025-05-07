7cb129431964901fe554643ba5e07d7ef2ba810f
ajay9875 <kumar.ajaysah2003@gmail.com>
2025-05-07 15:58:21 +0530

    Style of form and recommendation updated at the dashborad.

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config("SECRET_KEY")
 #print(SECRET_KEY)
 
 # Ensure DEBUG is True for development not for deployment
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #ALLOWED_HOSTS = []
