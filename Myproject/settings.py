117fbb4911d622ef93cc4495326d0e5232b15637
ajay9875 <kumar.ajaysah2003@gmail.com>
2025-06-17 18:17:26 +0530

    Database locking problem solved

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -85,6 +85,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            'timeout': 20,
+        },
+        'ATOMIC_REQUESTS': True,
     }
 }
 
